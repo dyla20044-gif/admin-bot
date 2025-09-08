21a367a6257c13ddbb6d98eb09a6d8d34a612826
dyla20044-gif <dyla20044@gmail.com>
2025-09-08 14:52:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ NEWS_API_KEY = os.getenv("NEWS_API_KEY") # Nueva variable
 # ----------------------------------------
 
 # Channel ID
-TELEGRAM_CHANNEL_ID = -1002139779491
+TELEGRAM_CHANNEL_ID = --1002139779491
 BASE_TMDB_URL = "https://api.themoviedb.org/3"
 POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500"
 TRAKT_BASE_URL = "https://api.trakt.tv"
@@ -1485,3 +1485,4 @@ async def main():
         
 if __name__ == "__main__":
     asyncio.run(main())
+
